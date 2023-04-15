35d4ac8143b5c8850c0eaba7132ad8b0ff170d76
elvis <elv.velasquez@duocuc.cl>
2023-04-15 10:24:40 -0400

    index listo

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+var rut = document.getElementById('rut');
 var nombre = document.getElementById('nombre');
-var password = document.getElementById('password');
+var apellido_paterno = document.getElementById('apellido paterno');
+var apellido_materno = document.getElementById('apellido_materno');
+var fecha_nacimiento = document.getElementById('fecha_nacimiento');
+var edad = document.getElementById('edad');
+var genero = document.getElementById('genero');
+var email = document.getElementById('email');
+var celular = document.getElementById('celular');
+var profesion = document.getElementById('profesion');
+var comentario = document.getElementById('comentario');
 var error = document.getElementById('error');
 error.style.color = 'red';
 
